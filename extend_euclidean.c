913a6ebc63b1ff0cf4e5baafd19c16482b63e272
Michael-Santos <31448704+Michael-Santos@users.noreply.github.com>
2019-10-12 21:10:10 -0300

    Add files via upload

--- extend_euclidean.c
+++ extend_euclidean.c
@@ -5,40 +5,53 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-typedef unsigned long long int NUMBER;
+typedef long long int NUMBER;
 
 /*
- * Calculate greatest divisor common of two numbers
+ * Calculate modular inverse of gdc(a, b)
  * 
  * @params a - First number
  * @params b - Second number
- * @returns Greatest divisor common of a and b
+ * @returns Greatest modular inverse of a mod b
 */
 NUMBER extended_euclidean(NUMBER a, NUMBER b) {
-    NUMBER remainder1, remainder2, quotient, aux;
+    NUMBER remainder1, remainder2, s, t, s1, s2, t1, t2, quotient, aux;
+    int i;
 
     remainder1 = a;
     remainder2 = b;
+    s1 = 1;
+    s2 = 0;
+    t1 = 0;
+    t2 = 1;
+    i = 0;
 
     do {
         quotient = remainder1 / remainder2;
         aux = remainder1 % remainder2;
         remainder1 = remainder2;
         remainder2 = aux;
+        
+        s = s1 - quotient * s2;
+        s1 = s2;
+        s2 = s;
+        t = t1 - quotient * t2;
+        t1 = t2;
+        t2 = t;
 
-        if (remainder2 == 0) return quotient;
     } while(remainder2 != 0);
     
-    return 0;
+    return t1;
 }
 
 int main()
 {
     NUMBER number1, number2;
 
-    scanf("%llu", &number1, &number2);
+    scanf("%lld", &number1);
+    scanf("%lld", &number2);
 
-    printf("%llu\n", extended_euclidean(42, 38));
+    printf("%lld\n", extended_euclidean(number1, number2));
 
     return 0;
 }
